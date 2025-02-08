bab06a4f35a5d10261e692e7e05202d7a16d0103
Mahnoor Shaikh <mahnoorshaikh1066@gmail.com>
2025-02-08 12:05:20 +0500

    Admin Dashboard foodtuck

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import { client } from "@/sanity/lib/client";
 import Image from "next/image";
-import ProtectedRoute from "@/app/component/protected/page";
+import ProtectedRoute from "@/app/component/protected/ProtectedRoute";
 
 interface Order {
   _id: string;
